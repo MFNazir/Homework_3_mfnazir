34d4bef951311d5b0095c590fbb7c8808542b326
Maryam <mnazir9876@gmail.com>
2019-10-25 18:04:05 -0500

    Added JQuery + Cocoon+ Fixed Forms

--- app/models/physician.rb
+++ app/models/physician.rb
@@ -1,5 +1,6 @@
 class Physician < ApplicationRecord
-    has_many :offices
+    has_many :offices, inverse_of: :physician
     has_many :appointments
     has_many :patients, through: :appointments
+    accepts_nested_attributes_for :offices, reject_if: :all_blank, allow_destroy: true
 end
